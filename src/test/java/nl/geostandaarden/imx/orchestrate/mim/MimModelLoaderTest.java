45931681a56f6f8a65fac69e0c6a727d002e73c3
Joost Farla <joost@qonduct.io>
2023-10-12 12:23:22 +0200

    Adapt to package change

--- src/test/java/nl/geostandaarden/imx/orchestrate/mim/MimModelLoaderTest.java
+++ src/test/java/nl/geostandaarden/imx/orchestrate/mim/MimModelLoaderTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
-import nl.geostandaarden.imx.orchestrate.ext.spatial.GeometryTypeFactory;
+import nl.geostandaarden.imx.orchestrate.ext.spatial.geometry.GeometryTypeFactory;
 import nl.geostandaarden.imx.orchestrate.model.types.ValueTypeRegistry;
 import nl.geostandaarden.mim.parser.xml.ModelParser;
 import org.junit.jupiter.api.Test;
